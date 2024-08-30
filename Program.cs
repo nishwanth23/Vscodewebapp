fb40a75b604fdb2e396f94c4bb4622bddd8d4271
nishwanth23 <149841176+nishwanth23@users.noreply.github.com>
2024-08-30 19:57:53 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,17 +9,18 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
+}
+else
+{
+    // In Development, you can choose to skip HTTPS redirection if not necessary
+    // app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
 
 app.Run();
